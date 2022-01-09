a517376e3cd5bc5b702bab69feebc2425d78a30b
Abhi <abhishek3.mk@gmail.com>
2022-01-09 16:57:57 +0530

    My develop indian

--- src/test/java/practise/AppiumFramework/GitDemoChanges.java
+++ src/test/java/practise/AppiumFramework/GitDemoChanges.java
@@ -10,4 +10,11 @@ public class GitDemoChanges {
 	{
 		System.out.println("Git Demo One uploaded by indian");
 	}
+	
+	@Test
+	public void gitDemotwo()
+	{
+		System.out.println("Git Demo Two uploaded by indian");
+	}
+	
 }
