d324fd242780ce2d9cdc5cbe31f7fc227e270e6f
Abhi <abhishek3.mk@gmail.com>
2022-01-09 13:35:25 +0530

    This is an American guy

---
+++ src/test/java/practise/AppiumFramework/GitAppiumChanges.java
@@ -0,0 +1,15 @@
+package practise.AppiumFramework;
+
+public class GitAppiumChanges {
+	
+	public void gitAppiumOne()
+	{
+		System.out.println("git Appium One");
+	}
+
+	public void gitAppiumTwo()
+	{
+		System.out.println("git Appium two uploded to git by American");
+	}
+
+}
