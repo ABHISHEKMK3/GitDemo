f247c11638790b48f77d84fe8afa7bf8e28696c9
Abhi <abhishek3.mk@gmail.com>
2022-01-30 23:51:08 +0530

    GitDemo commit of DemoNight from master at 11/12 to stage

---
+++ src/test/java/practise/AppiumFramework/DemoNight.java
@@ -0,0 +1,12 @@
+package practise.AppiumFramework;
+
+public class DemoNight {
+	
+	
+	public void workedAt11()
+	{
+		System.out.println("Git Demo Master uploded at 11");
+		System.out.println("Git Demo Master uploded at 12");
+	}
+
+}
